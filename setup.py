5bbd30764f05f2cb164657da0f7e7a359eb437e2
Madhura <madhura9904@gmail.com>
2025-02-16 23:40:09 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:#Read all packages in requirements
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+
+    return requirements
+
+
+
+setup(
+
+    name='DiamondPricePrediction',
+    version='0.0.1',
+    author='pwskills',
+    author_email='pw@gmailcom',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+)
