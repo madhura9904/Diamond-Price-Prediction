395ddb79ff3a3269248250f8acab1ec23533b8f1
Madhura <madhura9904@gmail.com>
2025-02-24 21:57:31 +0530

    Project done

--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -1,30 +1,26 @@
+import pandas as pd
 import os
 import sys
-
-project_root = r"C:\Diamond Price Prediction"
-
-if project_root not in sys.path:
-    sys.path.append(project_root)
-
-print(sys.path)
 from src.logger import logging
 from src.exception import CustomException
-import pandas as pd
-
-
-
-
 
 from src.components.data_ingestion import DataIngestion
+
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 if __name__=='__main__':
     obj=DataIngestion()
     train_data_path,test_data_path=obj.initiate_data_ingestion()
-
     print(train_data_path,test_data_path)
 
     data_transformation=DataTransformation()
-    train_arr,test_arr,obj_path=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
 
-    
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+
+    model_trainer=ModelTrainer()
+
+    model_trainer.initate_model_training(train_arr,test_arr)
+
+#python -m src.pipelines.training_pipeline
